4a71c64a3d90d2b3811b163f4b8520fb57a897e8
nuca <nuca@nucas-MacBook-Pro.local>
2024-03-20 23:47:29 +0400

    change-task1-assignment1Algorithmic

--- algorithmic-assignments/assignment1.playground/Contents.swift
+++ algorithmic-assignments/assignment1.playground/Contents.swift
@@ -9,18 +9,26 @@ import UIKit
 func haveSameCharacters(line1: String, line2: String) -> Bool {
     var dictionaryLine1 = [Character: Int]()
     for char in line1 {
-        dictionaryLine1[char] = 0
-    }
-    for char in line1 {
-        dictionaryLine1[char]! += 1
+        if (dictionaryLine1[char] == nil) {
+            dictionaryLine1[char] = 0
+        } else {
+            dictionaryLine1[char]! += 1
+        }
     }
     var dictionaryLine2 = [Character: Int]()
     for char in line2 {
-        dictionaryLine2[char] = 0
-    }
-    for char in line2 {
-        dictionaryLine2[char]! += 1
+        if (dictionaryLine2[char] == nil) {
+            dictionaryLine2[char] = 0
+        } else {
+            dictionaryLine2[char]! += 1
+        }
     }
     return dictionaryLine1 == dictionaryLine2
 }
-haveSameCharacters(line1: "abc", line2: "aacb")
+haveSameCharacters(line1: "abc", line2: "cb")
+
+/* დაწერეთ ფუნქცია რომელიც მიიღებს და დააბრუნებს სტრინგს სადაც ზედმეტ სფეისებს მოაშორებს.
+ 
+ მაგ:
+ "A[space][space][space]B[space][space]C" დააბრუნებს "A[space]B[space]C"
+ "[space][space][space]a" დააბრუნებს "[space]a" ანუ 1 სფეისი ყოველთვის რჩება, ამ შემთხვევაში წინაც. */
