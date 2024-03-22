3f116a2e570b6b848262f959035de5cf3782ab2b
nuca <nuca@nucas-MacBook-Pro.local>
2024-03-22 13:53:11 +0400

    finish-task3-assignment6

--- assignment6.playground/Contents.swift
+++ assignment6.playground/Contents.swift
@@ -41,3 +41,57 @@ enum GialaSquad {
 
 var nuca = GialaSquad.TuGapatio
 nuca.printDonePrice()
+
+/* შექმენით enum-ი Weather შემდეგი ქეისებით, Sunny, Cloudy, Rainy და Snowy. ამ ქეისებს უნდა ჰქონდეს associated value Celsius-ის სახით. დაწერეთ function რომელიც მიიღებს ამ enum-ს, და მოგვცემს რეკომენდაციას რა უნდა ჩავიცვათ შესაბამისი ამინდის მიხედვით. */
+enum Weather {
+    case Sunny(celcius: Int)
+    case Cloudy(celcius: Int)
+    case Rainy(celcius: Int)
+    case Snowy(celcius: Int)
+}
+
+func giveRecomendation(weather: Weather) -> () {
+    switch weather {
+    case .Sunny(let celcius):
+        if celcius > 23 {
+            print("wear light clothes")
+        } else if celcius > 13 {
+            print("bring a jacket")
+        } else if celcius > 0 {
+            print("puffer jacket weather")
+        } else {
+            print("wear the warmest clothes")
+        }
+    case .Cloudy(let celcius):
+        if celcius > 27 {
+            print("wear light clothes")
+        } else if celcius > 15 {
+            print("bring a jacket")
+        } else if celcius > 5 {
+            print("puffer jacket weather")
+        } else {
+            print("wear the warmest clothes")
+        }
+    case .Rainy(let celcius):
+        if celcius > 27 {
+            print("wear light clothes and bring an umbrella")
+        } else if celcius > 15 {
+            print("bring a jacket and an umbrella")
+        } else if celcius > 5 {
+            print("it's puffer jacket weather, bring an umbrella")
+        } else if celcius > 0 {
+            print("wear the warmest clothes and bring an umbrella")
+        } else {
+            print("wrong data")
+        }
+    case .Snowy(let celcius):
+        if celcius < 0 {
+            print("wear a snowsuit and snowboots")
+        } else {
+            print("wrong data")
+        }
+    }
+}
+
+var weather = Weather.Cloudy(celcius: 30)
+giveRecomendation(weather: weather)
